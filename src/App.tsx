92efd076759a11aedb38fa1d2ee7ec68ac8554bd
Bharath Raja <bharathraja@live.com>
2020-02-04 07:14:19 +0530

    Basic typescript react base

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Game from './game'
+
+function App() {
+  return (
+    <div className="App">
+      ....
+    </div>
+  );
+}
+
+export default App
